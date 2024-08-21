9fd25b8f929574fe7627095808fc8a9ba8a9d829
Nahian2005 <118044869+Nahian2005@users.noreply.github.com>
2024-08-21 18:13:50 +0600

    Add files via upload

---
+++ rim.js
@@ -0,0 +1,150 @@
+
+
+
+
+document.querySelectorAll(".mybutton")[0].addEventListener("mouseover", function(){
+
+
+
+var text=this.innerHTML;
+
+document.querySelector("h1").innerHTML=text+"(Lenju)";
+
+
+
+});
+
+
+
+
+
+
+    document.querySelectorAll(".mybutton")[1].addEventListener("mouseover", function(){
+    
+    
+    
+    var text=this.innerHTML;
+    
+    document.querySelector("h1").innerHTML=text+"(Tui)";
+    
+    
+    
+    });
+    
+    
+
+
+
+
+
+    document.querySelectorAll(".mybutton")[2].addEventListener("mouseover", function(){
+    
+    
+    
+    var text=this.innerHTML;
+    
+    document.querySelector("h1").innerHTML=text+"(TAratari)";
+    
+    
+    
+    });
+    
+   
+
+
+
+
+
+    document.querySelectorAll(".mybutton")[3].addEventListener("mouseover", function(){
+    
+    
+    
+    var text=this.innerHTML;
+    
+    document.querySelector("h1").innerHTML=text+"(ASH)";
+    
+    
+    
+    });
+    
+    
+
+
+
+
+
+    document.querySelectorAll(".mybutton")[4].addEventListener("mouseover", function(){
+    
+    
+    
+    var text=this.innerHTML;
+    
+    document.querySelector("h1").innerHTML=text+"(Nahole)";
+    
+    
+    
+    });
+    
+
+
+
+
+
+    document.querySelectorAll(".mybutton")[5].addEventListener("mouseover", function(){
+    
+    
+    
+    var text=this.innerHTML;
+    
+    document.querySelector("h1").innerHTML=text+"(Tor)";
+    
+    
+    
+    });
+    
+ 
+
+
+
+    document.querySelectorAll(".mybutton")[6].addEventListener("mouseover", function(){
+    
+    
+    
+    var text=this.innerHTML;
+    
+    document.querySelector("h1").innerHTML=text+"(Hogai)";
+    
+    
+    
+    });
+    
+    
+
+
+
+
+
+    document.querySelectorAll(".mybutton")[7].addEventListener("mouseover", function(){
+    
+    
+    
+    var text=this.innerHTML;
+    
+    document.querySelector("h1").innerHTML=text+"(Latthi Marmo)";
+    
+    
+    
+    });
+    
+    
+
+
+
+
+
+
+
+
+
+
+
